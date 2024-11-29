7075797df8242688b7041a556afb882cae8589f0
David Taylor <david@taylorhq.com>
2024-11-29 15:40:30 +0000

    DEV: Convert to native class syntax (#28)

--- javascripts/discourse/components/dismissable-banner.js
+++ javascripts/discourse/components/dismissable-banner.js
@@ -1,40 +1,41 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
+import { tagName } from "@ember-decorators/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
-export default Component.extend({
-  tagName: "",
-  hidden: true,
+@tagName("")
+export default class DismissableBanner extends Component {
+  hidden = true;
 
   didInsertElement() {
-    this._super(...arguments);
+    super.didInsertElement(...arguments);
     this.appEvents.on("cta:shown", this, this._triggerBanner);
-  },
+  }
 
   willDestroyElement() {
-    this._super(...arguments);
+    super.willDestroyElement(...arguments);
     this.appEvents.off("cta:shown", this, this._triggerBanner);
-  },
+  }
 
   _triggerBanner() {
     this.set("hidden", false);
-  },
+  }
 
   @discourseComputed("hidden")
   shouldShow(hidden) {
     return !hidden;
-  },
+  }
 
   @action
   dismissBanner() {
     this.keyValueStore.setItem("anon-cta-never", "t");
     this.session.set("showSignupCta", false);
     this.set("hidden", true);
-  },
+  }
 
   @action
   showBannerLater() {
     this.keyValueStore.setItem("anon-cta-hidden", Date.now());
     this.set("hidden", true);
-  },
-});
+  }
+}
